00b7ab87d7ed761f39e6332a3a4094385b524268
TylerKellogg <yaz8bp@virginia.edu>
2026-05-25 23:06:48 +0000

    saving our two init files

---
+++ scripts/init_git_creds.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+USER=yaz8bp@virginia.edu
+NAME=TylerKellogg
+
+git config --global --list
+
+git config --global user.email ${USER}
+git config --global user.name ${NAME}
+
+git config --global --list
